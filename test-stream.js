01dbde10285a1844dfc7924754555f210844ec20
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-07-12 14:14:28 +0700

    Update test-stream.js

--- test-stream.js
+++ test-stream.js
@@ -1,1 +1,1 @@
-window.location.href = 'https://vips-livecdn.fptplay.net/hda1/vtv1hd_vhls.smil/chunklist_b5000000.m3u8'
+location.href = "https://vips-livecdn.fptplay.net/hda1/vtv1hd_vhls.smil/chunklist_b5000000.m3u8"
